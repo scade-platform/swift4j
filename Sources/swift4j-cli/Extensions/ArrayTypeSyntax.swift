0ccb781034912e9039cbf3d577cf0c5d4aa32e56
Grigory Markin <gr.markin@gmail.com>
2026-02-11 19:14:32 +0100

    fix Kotlin type names for arrays of primitive types

--- Sources/swift4j-cli/Extensions/ArrayTypeSyntax.swift
+++ Sources/swift4j-cli/Extensions/ArrayTypeSyntax.swift
@@ -5,13 +5,20 @@ import SwiftSyntaxExtensions
 
 
 extension ArrayTypeSyntax: MappableTypeSyntax {
+  private static let primitives: Set<String> = ["Boolean", "Long", "Int", "Short", "Byte", "Float", "Double"]
+
   func map(with ctx: inout ProxyGenerator.Context, primitivesAsObjects: Bool) -> String {
     let elementMap = element.map(with: &ctx, primitivesAsObjects: primitivesAsObjects)
     switch ctx.settings.language {
       case .java:
         return elementMap + "[]"
+
       case .kotlin:
-        return "Array<\(elementMap)>"
+        if ArrayTypeSyntax.primitives.contains(elementMap) {
+          return "\(elementMap.prefix(1).uppercased() + elementMap.dropFirst())Array"
+        } else {
+          return "Array<\(elementMap)>"
+        }
     }
 
   }
